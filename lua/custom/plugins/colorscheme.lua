bf52523069a836f25b8644719603f46cdd06fbee
mc-stephen <stephenugo939@gmail.com>
2026-07-07 16:45:29 +0100

    Made some changes

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -14,3 +14,11 @@ vim.pack.add {
   -- 5. Kanagawa (Inspired by traditional Japanese art, very easy on the eyes)
   'https://github.com/rebelot/kanagawa.nvim',
 }
+
+-- Making It Permanent
+-- Whenever you find a variant that hits perfectly, go ahead and replace your old cmd('colorscheme tokyonight') line in your init.lua with the new name. For example:
+-- If you choose the clean GitHub variant
+-- vim.cmd('colorscheme github_dark_dimmed')
+
+-- Or if you want to try out Catppuccin
+-- vim.cmd('colorscheme catppuccin-mocha')
